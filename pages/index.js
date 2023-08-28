4739d30e73669ef69c95dba35ac6d9c079a478a7
Johann <40876495+joFolta@users.noreply.github.com>
2023-08-28 15:54:41 -0400

    Dynamic Routes with getStaticPaths

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Date from '../components/date';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
@@ -22,9 +23,6 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hi, I'm Johann - software engineer 👨🏻‍💻 from Boston specializing in React, Node, and Python. When I'm not coding, I enjoy hiking in the White Mountains of New Hampshire, riding my 400cc naked motorcycle, and mountain biking in the trails. I live with my fellow-engineer wife, curious-about-life toddler, and rambunctious black cat 🐈‍.</p>
-        <p>
-          &gt; Read <Link href="/posts/first-post">this page!</Link>
-        </p>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
@@ -32,11 +30,11 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
